f92dd6db4b545b6d6e0c94ebcd86d5bcb108e093
yago.sodre <yago.sousa@estuda.com>
2023-08-16 18:01:37 -0300

    refactor: dependent-tests

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,38 +1,44 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
+    cy.visit('http://notes-serverless-app.com/login')
 
-    cy.get('.navbar-nav a:contains(Login)').click()
-
-    cy.get('#email').type(Cypress.env('user_email'))
-    cy.get('#password').type(Cypress.env('user_password'), { log: false })
+    cy.get('#email').type(Cypress.env('another_user_email'))
+    cy.get('#password').type(Cypress.env('another_user_password'), { log: false })
     cy.get('button[type="submit"]').click()
     
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
+    //Create a note
     cy.contains('Create a new note').click()
-
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
+    //Assert the note was created
+    cy.get('.list-group')
+      .contains('My note')
+      .click()
 
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
+    //Updates the note
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
+    //Asserts the note was update
     cy.get('.list-group').should('contain', 'My note updated')
     cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
+      
 
-  it('deletes a note', () => {
+    //Deletes the note
     cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
 
+    //Asserts the list has at least one item before asserting the note deletion
+    //This way, we make sure the list of notes has already rendered
+    cy.get('.list-group a')
+      .its('length')
+      .should('be.at.least', 1)
+    //Asserts the note was deleted
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
 })
