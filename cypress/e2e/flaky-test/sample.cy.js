8922551ea92c7c2d45529b9fdd307180bbf1fcb1
yago.sodre <yago.sousa@estuda.com>
2023-08-16 11:16:17 -0300

    refactor: flaky test

--- cypress/e2e/flaky-test/sample.cy.js
+++ cypress/e2e/flaky-test/sample.cy.js
@@ -2,12 +2,13 @@ import { faker } from '@faker-js/faker'
 
 describe('Flaky tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('https://wlsf82-hacker-stories.web.app')
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    )
 
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
+    cy.visit('https://wlsf82-hacker-stories.web.app')
   })
 
   Cypress._.times(10, () => {
@@ -15,11 +16,6 @@ describe('Flaky tests bad practice', () => {
       Cypress._.times(6, () => {
         cy.search(faker.random.word())
       })
-
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
 
       cy.get('.last-searches button')
         .should('have.length', 5)
